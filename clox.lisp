1f64e6360a8980d161a910ba7a019de4ed2de6d1
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2020-02-16 22:16:05 +0100

    Add some high level code
    Plenty of stuff not implemented yet.

--- clox.lisp
+++ clox.lisp
@@ -1,3 +1,23 @@
 ;;;; clox.lisp
 
 (in-package #:clox)
+
+(defparameter *had-error-p* nil)
+
+(defun prompt ()
+  (loop (print "> ")
+        (run (read-line))
+        (setf *had-error-p* nil)))
+
+(defun run (source)
+  (let* ((scanner (make-scanner source))
+         (tokens (scan-tokens scanner)))
+    (loop for token = tokens
+          (format t "~a~%" token))))
+
+(defun error% (line message)
+  (report line "" message))
+
+(defun report% (line where message)
+  (format t "[line ~a] Error ~a: ~a~%" line where message)
+  (setf *had-error-p* t))
